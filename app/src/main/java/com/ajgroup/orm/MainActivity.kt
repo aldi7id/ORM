24b44fced1c65d4ed2ad3443afd2e557e92e386d
aldi7id <aldi7.indo@gmail.com>
2022-04-01 21:06:34 +0700

    edit MainActivity.kt

--- app/src/main/java/com/ajgroup/orm/MainActivity.kt
+++ app/src/main/java/com/ajgroup/orm/MainActivity.kt
@@ -1,14 +1,50 @@
 package com.ajgroup.orm
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.ajgroup.orm.databinding.ActivityMainBinding
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    private var mDB: StudentDatabase? = null
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        mDB = StudentDatabase.getInstance(this)
+
+        binding.recycleView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+
+        fetchData()
+        binding.fabAdd.setOnClickListener {
+            val keActivityAdd = Intent(this, AddActivity::class.java)
+            startActivity(keActivityAdd)
+        }
+    }
+    override fun onResume() {
+        super.onResume()
+        fetchData()
+    }
+    private fun fetchData() {
+        GlobalScope.launch {
+            val listStudent = mDB?.studentDao()?.getAllStudent()
+
+            runOnUiThread {
+                listStudent?.let {
+                    val adapter = StudentAdapter(it)
+                    binding.recycleView.adapter = adapter
+                }
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        StudentDatabase.destroyInstance()
     }
 }
