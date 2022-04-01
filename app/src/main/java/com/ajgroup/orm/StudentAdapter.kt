95f0f7e836ec8c582c5ac069a06b675430e8cb56
aldi7id <aldi7.indo@gmail.com>
2022-04-01 20:23:09 +0700

    add addactivity & editactivity

--- app/src/main/java/com/ajgroup/orm/StudentAdapter.kt
+++ app/src/main/java/com/ajgroup/orm/StudentAdapter.kt
@@ -4,12 +4,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.ajgroup.orm.databinding.StudentItemBinding
 
 class StudentAdapter(val listStudent: List<Student>): RecyclerView.Adapter<StudentAdapter.ViewHolder>() {
-    private lateinit var binding: StudentAdapter
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    class ViewHolder(val binding:StudentItemBinding) : RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val itemView = LayoutInflater
+        val binding = StudentItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return ViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return listStudent.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.binding.tvId.text = listStudent[position].id.toString()
+        holder.binding.tvName.text = listStudent[position].nama
+        holder.binding.tvEmail.text = listStudent[position].email
+
     }
 }
