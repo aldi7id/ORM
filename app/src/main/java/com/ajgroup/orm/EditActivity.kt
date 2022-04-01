9e2d029771971166e672994919a8da5d749efd05
aldi7id <aldi7.indo@gmail.com>
2022-04-01 21:42:50 +0700

    edit EditActivity.kt

--- app/src/main/java/com/ajgroup/orm/EditActivity.kt
+++ app/src/main/java/com/ajgroup/orm/EditActivity.kt
@@ -2,10 +2,42 @@ package com.ajgroup.orm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.ajgroup.orm.databinding.ActivityEditBinding
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.runBlocking
 
 class EditActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityEditBinding
+    var mDb: StudentDatabase? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit)
+        binding = ActivityEditBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        mDb = StudentDatabase.getInstance(this)
+        val objectStudent = intent.getParcelableExtra<Student>("student")
+
+        binding.etNamaStudent.setText(objectStudent?.nama)
+        binding.etEmailStudent.setText(objectStudent?.email)
+
+        binding.btnSave.setOnClickListener {
+            objectStudent?.nama = binding.etNamaStudent.text.toString()
+            objectStudent?.email = binding.etEmailStudent.text.toString()
+
+            GlobalScope.async {
+                val result = mDb?.studentDao()?.updateStudent(objectStudent!!)
+
+                runBlocking {
+                    if (result !=0){
+                        Toast.makeText(this@EditActivity, "Sukses mengubah ${objectStudent?.nama}", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(this@EditActivity, "Gagal mengubah ${objectStudent?.nama}", Toast.LENGTH_SHORT).show()
+                    }
+                    finish()
+                }
+            }
+        }
+
     }
 }
