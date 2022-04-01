1fce628f73bec17d002f3b9630cd698bcc174908
aldi7id <aldi7.indo@gmail.com>
2022-04-01 19:07:54 +0700

    add studentdao class

---
+++ app/src/main/java/com/ajgroup/orm/StudentDao.kt
@@ -0,0 +1,19 @@
+package com.ajgroup.orm
+
+import android.arch.persistence.room.*
+import android.arch.persistence.room.OnConflictStrategy.REPLACE
+
+@Dao
+interface StudentDao {
+    @Query ("SELECT * FROM Student")
+    fun getAllStudent(): List<Student>
+
+    @Insert(onConflict = REPLACE)
+    fun insertStudent(student: Student):Long
+
+    @Update
+    fun updateStudent(student: Student):Int
+
+    @Delete
+    fun deleteStudent(student: Student):Int
+}
